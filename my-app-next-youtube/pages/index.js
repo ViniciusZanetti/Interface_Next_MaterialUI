20f01bc3a863cfa3970a979eeed56ac514749f83
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-11-11 11:08:38 -0300

    nada renderizado ainda

--- my-app-next-youtube/pages/index.js
+++ my-app-next-youtube/pages/index.js
@@ -1,9 +1,16 @@
-
+import React from "react";
+import {Button, Container} from "@material-ui/core"
 
 export default function Home() {
   return (
     <div>
       Criando clone youtube Next.js + Material UI
+      
+    <Container>
+      <Button variant="contained" color="primary" label="teste"/>
+    </Container>
+
     </div>
+    
   )
 }
